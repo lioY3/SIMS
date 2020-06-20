1dae8f58fa11dfc4c1bdeaa5d8ffa568563a6e3f
Chen <974098492@qq.com>
2020-06-20 23:32:07 +0800

    解决冲突

--- src/model/User.java
+++ src/model/User.java
@@ -2,37 +2,6 @@ package model;
 
 import java.io.Serializable;
 
-<<<<<<< HEAD
-public class User implements Serializable{
-
-	private static final long serialVersionUID = 1L;
-	private String username;//用户名
-	private String password;//密码
-	private String level;//权限级别
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getLevel() {
-		return level;
-	}
-	public void setLevel(String level) {
-		this.level = level;
-	}
-	
-
-}
-
-=======
-
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -60,4 +29,4 @@ public class User implements Serializable{
 	
 
 }
->>>>>>> branch 'master' of https://gitee.com/chen295/student_information_management.git
+
