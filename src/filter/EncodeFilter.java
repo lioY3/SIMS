8d7f50244f29969494d8f19be0de84b50fbe9b2c
Chen <974098492@qq.com>
2020-06-20 22:22:43 +0800

    upload

--- src/filter/EncodeFilter.java
+++ src/filter/EncodeFilter.java
@@ -7,46 +7,46 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet Filter implementation class EncodeFilter
- */
-//@WebFilter("/*")
-//暂时屏蔽
 public class EncodeFilter implements Filter {
 
-    /**
-     * Default constructor. 
-     */
     public EncodeFilter() {
-        // TODO Auto-generated constructor stub
+        
     }
 
-	/**
-	 * @see Filter#destroy()
-	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
+		
 	}
 
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	/*
+	 * public void doFilter(ServletRequest request, ServletResponse response,
+	 * FilterChain chain) throws IOException, ServletException {
+	 * request.setCharacterEncoding("UTF-8"); chain.doFilter(request, response);
+	 * 
+	 * }
 	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+	public void doFilter(ServletRequest req, ServletResponse rep, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) rep;
 		//设置编码格式
-		request.setCharacterEncoding("UTF-8");
-		//设置返回类型
-		response.setContentType("text/html; charset=utf-8");
-		//放行资源
-		chain.doFilter(request, response);
+		String a = request.getRequestURI();
+        if(a.contains(".css") || a.contains(".js") || a.contains(".png")|| a.contains(".jpg")){
+            //如果发现是css或者js文件，直接放行
+        	chain.doFilter(request, response);
+        }
+        else {
+        	request.setCharacterEncoding("utf-8");
+        	response.setContentType("text/html; charset=utf-8");
+        	
+        }
+        chain.doFilter(request, response);
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
 	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
