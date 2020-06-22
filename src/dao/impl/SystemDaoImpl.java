454b5b108ad62add72a18789eae867b95fe4c73d
81 <1289209158@qq.com>
2020-06-22 18:48:29 +0800

    loginservlet

---
+++ src/dao/impl/SystemDaoImpl.java
@@ -0,0 +1,18 @@
+package dao.impl;
+
+import java.sql.Connection;
+
+import dao.impl.BaseDaoImpl;
+import dao.inter.SystemDaoInter;
+
+/**
+ * 系统数据层
+ *
+ */
+public class SystemDaoImpl extends BaseDaoImpl implements SystemDaoInter {
+
+	
+	
+	
+	
+}
--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -1,10 +1,10 @@
 package servlet;
 
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,34 +13,58 @@ import model.User;
 import service.SystemService;
 import tool.VCodeGenerator;
 
+import java.awt.image.BufferedImage;
 
 /**
  * 验证码
  *
  */
-public class LoginServlet extends HttpServlet {
 
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	private SystemService service = new SystemService();
 	
+	private SystemService service = new SystemService();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		//获取请求的方法
 		String method = request.getParameter("method");
-		
+				
 		if("GetVCode".equalsIgnoreCase(method)){
 			getVCode(request, response);
 		}
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		//获取请求的方法
 		String method = request.getParameter("method");
-		
+				
 		if("Login".equals(method)){ //验证登录
 			login(request, response);
 		}
+		//doGet(request, response);
 	}
+	
 	
 	
 	/**
@@ -112,5 +136,6 @@ public class LoginServlet extends HttpServlet {
 		//输出图像
 		ImageIO.write(vImg, "gif", response.getOutputStream());
 	}
+
 
 }
