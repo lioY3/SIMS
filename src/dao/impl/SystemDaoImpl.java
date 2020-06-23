84ad6d1aa8aced47d82f9d5dd4c26b62963922a7
81 <1289209158@qq.com>
2020-06-23 13:52:02 +0800

    SystemDaoImpl

---
+++ src/dao/impl/SystemDaoImpl.java
@@ -0,0 +1,17 @@
+package dao.impl;
+
+import dao.BaseDao;
+import dao.impl.BaseDaoImpl;
+import dao.SystemDao;
+/**
+ * 系统数据层
+ *
+ */
+
+public class SystemDaoImpl extends BaseDaoImpl implements SystemDao {
+
+	
+	
+	
+	
+}
