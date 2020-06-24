7f2a57774ef242572a7327c54536102dac5bd851
81 <1289209158@qq.com>
2020-06-24 15:09:37 +0800

    ClassService

--- src/dao/ClassDao.java
+++ src/dao/ClassDao.java
@@ -1,17 +1,21 @@
 package dao;
 
+import java.util.List;
+
 /**
  * 班级数据层接口
  *
  */
 
 public interface ClassDao extends BaseDao {
+
+	@SuppressWarnings("rawtypes")
+	List<Class> getClazzDetailList(String clno);
 	
 	/**
 	 * 获取班级详细信息
 	 * @param Clno 班级ID
 	 * @return
 	 */
-	//public model.Class getClassDetail(String Clno);
 	
 }
